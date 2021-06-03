45c0affa16355c70ed01b057679d200d79ff82b1
liuchao <liuchao@sari-hk.com>
2021-06-03 09:48:20 +0800

    add sider text logo collapsed

--- src/components/text-logo/index.tsx
+++ src/components/text-logo/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SiderContext } from 'antd/es/layout/Sider';
 
 interface TextLogoProps extends React.HTMLAttributes<HTMLDivElement> {
-  
 }
 
 const TextLogo: React.FC<TextLogoProps> = function(props) {
+  const { siderCollapsed: collapsed } = useContext(SiderContext)
+
   return (
     <div className="font-sans text-xl leading-none text-white text-center h-16 py-5">
-      <b>R</b>eact Admin
+      {
+        collapsed ? (
+          <span><b>R</b>A</span>
+        ) : (
+          <span><b>R</b>eact Admin</span>
+        )
+      }
     </div>
   )
 }
+
 
 export default TextLogo
